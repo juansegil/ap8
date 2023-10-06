c3d3b9cf69dacd10c6f7c66fd61a95c09b0b01a6
juansegil <141371260+juansegil@users.noreply.github.com>
2023-10-06 09:40:33 -0500

    Add files via upload

---
+++ AP8/tiendalibros/modelo/tienda_libros.py
@@ -0,0 +1,16 @@
+class TiendaLibros:
+    pass
+    def __init__(self):
+        self.catalogo = {}
+        self.carrito = CarroCompras()
+
+    def adicionar_libro_a_catálogo(self, isbn:str, titulo:str, precio:float, existencias: int):
+        
+    
+    
+
+    # Defina metodo adicionar_libro_a_catalogo
+
+    # Defina metodo agregar_libro_a_carrito
+
+    # Defina metodo retirar_item_de_carrito
